7d6a3667827814326ec11383e6d8831dbbaffd67
izzypt <bonvalots@gmail.com>
2023-02-27 15:47:50 +0000

    Adding drf_spectacular/swagger to create API documentation

--- app/core/admin.py
+++ app/core/admin.py
@@ -11,15 +11,44 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
         (
-            ('Permissions'),
+            None, 
+            {'fields': (
+                'email', 
+                'password'
+                )
+            }
+        ),
+        (
+            ('Permissiones'),
             {'fields': (
                 'is_active', 
                 'is_staff', 
                 'is_superuser'
             )}
+        ),
+        (
+            ('Important Dates'),
+            {'fields': (
+                'last_login', 
+            )}
         )
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None,
+        {'fields':
+            (
+                'email', 
+                'password1', 
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }
+        ),
     )
 
 admin.site.register(models.User, UserAdmin)
